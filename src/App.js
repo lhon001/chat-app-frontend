c05d19da04eb739ab078e3ecf1a23b624b49521d
github username <lhon001@gmail.com>
2018-10-23 12:31:28 -0400

    chat app first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ConversationsList from './components/ConversationsList'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ConversationsList />
+      </div>
+    );
+  }
+}
+
+export default App;
